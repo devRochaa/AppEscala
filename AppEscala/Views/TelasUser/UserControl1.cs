1074e18e1c4444872f45f86f12bd55f4fa4ee639
Mauricio Pereira <124582086+devRochaa@users.noreply.github.com>
2025-03-22 19:11:39 -0300

    fix: formatação da escala, repetição e contagem de acólitos corrigidas
    Melhoria nas configurações visuais da escala; corrigido bug que repetia acólitos e exibia quantidade incorreta
    
    falta: não repetir acolito por semana e colocar escala para aparecer por ordem de dias

--- AppEscala/Views/TelasUser/UserControl1.cs
+++ AppEscala/Views/TelasUser/UserControl1.cs
@@ -66,15 +66,20 @@ namespace AppEscala
                 var document = new Document(pdfDocument, PageSize.A4);
 
                 //criar tabela
-                float[] columnWidth = { 15, 10, 35, 15, 25 };
+                float[] columnWidth = { 15, 10, 40, 10, 25 };
                 Table tabela = new Table(UnitValue.CreatePercentArray(columnWidth)).UseAllAvailableWidth();
 
                 //Título do cabeçalho da tabela
                 var fonte = PdfFontFactory.CreateFont(StandardFonts.HELVETICA);
+                var fonteNegrito = PdfFontFactory.CreateFont(StandardFonts.HELVETICA_BOLD);
 
-                var cellFotter = new Cell(1, 5).Add(new Paragraph("Escala dos acólitos"))
-                    .SetFont(fonte)
-                    .SetFontSize(13)
+                string day = DateTime.Now.Day.ToString();
+                string mes = DateTime.Now.ToString("MMMM", new System.Globalization.CultureInfo("pt-BR"));
+                mes = char.ToUpper(mes[0]) + mes.Substring(1);
+
+                var cellFotter = new Cell(1, 5).Add(new Paragraph("Escala dos acólitos " + day + " - " + mes ))
+                    .SetFont(fonteNegrito)
+                    .SetFontSize(10)
                     .SetFontColor(ColorConstants.BLACK)
                     .SetBackgroundColor(ColorConstants.WHITE)
                     .SetTextAlignment(TextAlignment.CENTER)
@@ -83,37 +88,40 @@ namespace AppEscala
                 tabela.AddHeaderCell(cellFotter);
 
 
-                tabela.AddHeaderCell(new Cell(1, 5).Add(new Paragraph("Escala dos acólitos")
-                    .SetFont(fonte)
-                    .SetFontSize(18)
-
-                    .SetFontColor(ColorConstants.BLUE)
-                    .SetBackgroundColor(ColorConstants.YELLOW)
-                    .SetTextAlignment(TextAlignment.CENTER)));
-                ;
+                
 
                 //Cabeçalho com os títulos das colunas da tabela
 
                 tabela.AddHeaderCell(new Cell()
+                    .SetFont(fonteNegrito)
+                    .SetFontSize(10)
                     .SetTextAlignment(TextAlignment.CENTER)
-                    .SetBackgroundColor(ColorConstants.LIGHT_GRAY)
+                    .SetBackgroundColor(ColorConstants.WHITE)
                     .Add(new Paragraph("DATA")));
                 tabela.AddHeaderCell(new Cell()
-                    .SetBackgroundColor(ColorConstants.LIGHT_GRAY)
+                    .SetFont(fonteNegrito)
+                    .SetFontSize(10)
+                    .SetBackgroundColor(ColorConstants.WHITE)
                     .SetTextAlignment(TextAlignment.CENTER)
                     .Add(new Paragraph("HORÁRIO")));
                 tabela.AddHeaderCell(new Cell()
-                    .SetBackgroundColor(ColorConstants.LIGHT_GRAY)
+                    .SetFont(fonteNegrito)
+                    .SetFontSize(10)
+                    .SetBackgroundColor(ColorConstants.WHITE)
                     .SetTextAlignment(TextAlignment.CENTER)
                     .Add(new Paragraph("ACÓLITOS")));
                 tabela.AddHeaderCell(new Cell()
+                    .SetFont(fonteNegrito)
+                    .SetFontSize(10)
                     .SetTextAlignment(TextAlignment.RIGHT)
-                    .SetBackgroundColor(ColorConstants.LIGHT_GRAY)
+                    .SetBackgroundColor(ColorConstants.WHITE)
                     .Add(new Paragraph(" ")));
                 tabela.SetSkipFirstHeader(false);
                 tabela.AddHeaderCell(new Cell()
+                    .SetFont(fonteNegrito)
+                    .SetFontSize(10)
                     .SetTextAlignment(TextAlignment.CENTER)
-                    .SetBackgroundColor(ColorConstants.LIGHT_GRAY)
+                    .SetBackgroundColor(ColorConstants.WHITE)
                     .Add(new Paragraph("LOCAL")));
                 tabela.SetSkipFirstHeader(false);
 
@@ -122,26 +130,26 @@ namespace AppEscala
                 foreach (Produtos prod in listaProdutos)
                 {
                     //primeira coluna data
-                    tabela.AddCell(new Cell().SetTextAlignment(TextAlignment.CENTER).Add(new
+                    tabela.AddCell(new Cell().SetTextAlignment(TextAlignment.CENTER).SetFontSize(9).Add(new
                         Paragraph(prod.data)));
 
                     //SEGUNDA COLUNA horario
-                    tabela.AddCell(new Cell().SetTextAlignment(TextAlignment.CENTER).Add(new
+                    tabela.AddCell(new Cell().SetTextAlignment(TextAlignment.CENTER).SetFontSize(9).Add(new
                         Paragraph(prod.horario)));
 
                     //TERCEIRA COLUNA acolitos
                     tabela.AddCell(new Cell()
-                        .SetTextAlignment(TextAlignment.CENTER).SetMinWidth(40).SetMaxWidth(50).Add(new
+                        .SetTextAlignment(TextAlignment.LEFT).SetMinWidth(40).SetMaxWidth(50).SetFontSize(9).Add(new
                         Paragraph(prod.acolitos)));
 
                     //QUARTA COLUNA evento
                     tabela.AddCell(new Cell()
-                        .SetTextAlignment(TextAlignment.CENTER).Add(new
+                        .SetTextAlignment(TextAlignment.CENTER).SetFontSize(9).SetFontColor(ColorConstants.RED).Add(new
                         Paragraph(prod.evento)));
 
                     //QUARTA COLUNA local
                     tabela.AddCell(new Cell()
-                        .SetTextAlignment(TextAlignment.CENTER).Add(new
+                        .SetTextAlignment(TextAlignment.CENTER).SetFontSize(9).Add(new
                         Paragraph(prod.local)));
                 }
 
@@ -172,48 +180,6 @@ namespace AppEscala
                 this.local = local;
             }
 
-
-            //    public static List<Produtos> GetListaProdutos()
-            //    {
-            //        //instanciar um objeto Lista de produtos
-            //        var relProdutos = new List<Produtos>();
-
-
-            //        relProdutos.Add(new Produtos("02-6ª.Feira", "19h30", "Daniel/Gabriel/João/Felipe", "", "Matriz"));
-            //        relProdutos.Add(new Produtos("22-Sábado", "09h00", "Luisa/Gabriela", "Novena", "Sto. Frei Galvão/Sta. Filomena"));
-            //        relProdutos.Add(new Produtos("01-Sábado", "08h00", "Ana/Marcos/João", "Matriz", "Cemitério"));
-            //        relProdutos.Add(new Produtos("03-Domingo", "07h00", "Carlos/Marina/Fernando", "", "São José"));
-            //        relProdutos.Add(new Produtos("04-2ª.Feira", "15h00", "Lucas/Sofia/Paulo", "Novena", "Matriz"));
-            //        relProdutos.Add(new Produtos("05-3ª.Feira", "19h30", "Mateus/Bruna/Renata", "", "Matriz"));
-            //        relProdutos.Add(new Produtos("06-4ª.Feira", "18h00", "Tiago/Júlia/Pedro", "Novena", "Sto. Frei Galvão"));
-            //        relProdutos.Add(new Produtos("07-5ª.Feira", "19h30", "André/Larissa/Felipe", "", "São Sebastião"));
-            //        relProdutos.Add(new Produtos("08-6ª.Feira", "20h00", "Débora/Rafael/Victor", "Novena", "Cristo Rei"));
-            //        relProdutos.Add(new Produtos("09-Sábado", "17h00", "Joana/Marcelo/Helena", "", "Santa Cecília"));
-            //        relProdutos.Add(new Produtos("10-Domingo", "09h00", "Carla/Thiago/Vinícius", "Novena", "Matriz"));
-            //        relProdutos.Add(new Produtos("11-2ª.Feira", "19h30", "Ester/Leonardo/Natalia", "", "Santa Catarina"));
-            //        relProdutos.Add(new Produtos("12-3ª.Feira", "18h30", "Aline/Ricardo/Gustavo", "Novena", "N.S. Graças"));
-            //        relProdutos.Add(new Produtos("13-4ª.Feira", "19h00", "Fernanda/Luiz/Amanda", "", "Cristo Rei"));
-            //        relProdutos.Add(new Produtos("14-5ª.Feira", "20h00", "Isabela/Renato/Clara", "Novena", "Sto. Frei Galvão"));
-            //        relProdutos.Add(new Produtos("15-6ª.Feira", "19h30", "Sérgio/Patrícia/Luana", "", "São José"));
-            //        relProdutos.Add(new Produtos("16-Sábado", "16h00", "Rodrigo/Mariana/Tânia", "Novena", "Santa Cecília"));
-            //        relProdutos.Add(new Produtos("17-Domingo", "18h00", "Igor/Rafaela/Davi/Clementina/Zé.Felipe/Daniel/Rocha/Oliveira", "", "N.S. Aparecida"));
-            //        relProdutos.Add(new Produtos("18-2ª.Feira", "19h30", "Camila/Fabiano/Flávia", "Novena", "São Sebastião"));
-            //        relProdutos.Add(new Produtos("19-3ª.Feira", "07h00", "Beatriz/Mateus/Rose", "", "Santa Catarina"));
-            //        relProdutos.Add(new Produtos("20-4ª.Feira", "18h30", "Juliano/Priscila/Samuel", "Novena", "Sto. Frei Galvão"));
-            //        relProdutos.Add(new Produtos("21-5ª.Feira", "19h00", "Viviane/Anderson/Lígia", "", "Cristo Rei"));
-            //        relProdutos.Add(new Produtos("23-6ª.Feira", "20h00", "Eduardo/Susana/Elisa", "Novena", "São José"));
-            //        relProdutos.Add(new Produtos("24-Sábado", "17h00", "Paulo/Letícia/Fernando", "", "Santa Cecília"));
-            //        relProdutos.Add(new Produtos("25-Domingo", "08h00", "Luciana/Ronaldo/Regina", "Novena", "N.S. Graças"));
-            //        relProdutos.Add(new Produtos("26-2ª.Feira", "19h30", "Henrique/Glória/Márcia", "", "São Sebastião"));
-            //        relProdutos.Add(new Produtos("27-3ª.Feira", "07h00", "Alexandre/Daniela/Eliane", "Novena", "Sto. Frei Galvão"));
-            //        relProdutos.Add(new Produtos("28-4ª.Feira", "18h30", "Vitor/Rosana/Danilo", "", "Cristo Rei"));
-            //        relProdutos.Add(new Produtos("29-5ª.Feira", "20h00", "Jéssica/Artur/Talita", "Novena", "São José"));
-            //        relProdutos.Add(new Produtos("30-Sexta", "19h00", "Gabriela/Emanuel/Caio", "", "N.S. Aparecida"));
-            //        relProdutos.Add(new Produtos("31-Sábado", "18h00", "Clarice/Osvaldo/Breno", "Novena", "Santa Cecília"));
-
-
-            //        return relProdutos;
-            //    }
             private static MySqlConnection Conexao;
             private static string data_source = "datasource=localhost;Port=3307;username=root;password=;database=escala_acolitos;";
             public string EncaixarAcolitos(string data, string horario, int quant)
@@ -297,34 +263,44 @@ namespace AppEscala
                     indice = qntAc;
                 }
                 Random randNum = new Random();
+                    
+                    List<int> lstIndexAcolitos = new List<int>();
+
                 for(int i = 0; i < indice; i++)
                 {
-                        int indexRand = randNum.Next(indice);
-                    MessageBox.Show($"indice: {indice} qntAC:{qntAc} randNUm: {indexRand} listaRNome : {lista[indexRand].Nome} acolitos: {acolitosQpodem} ");
-                    if (indice > 1 && i == 0)
+                    int indexRand;
+
+                    // Garante que o número seja único na lista
+                    do
                     {
-                        acolitosQpodem += lista[indexRand].Nome + "/";
-                    }
+                        indexRand = randNum.Next(indice);
+                    } while (lstIndexAcolitos.Contains(indexRand));
+
+                    lstIndexAcolitos.Add(indexRand);
+                    string strLista = string.Join(", ", lstIndexAcolitos);  
+                    //MessageBox.Show($"indice: {indice} qntAC:{qntAc} randNUm: {indexRand} listaRNome : {lista[indexRand].Nome} acolitos: {acolitosQpodem} listaIndex:{strLista} ");
+
                     if (indice == 1 || i + 1 == indice)
                     {
                         acolitosQpodem += lista[indexRand].Nome;
+                        break;
                     }
+
+                    if(i == 0)
+                    {
+                        acolitosQpodem += lista[indexRand].Nome + "/ ";
+                    }
+                    else
+                    {
+                        if (indice > 1)
+                        {
+                          acolitosQpodem += lista[indexRand].Nome + "/ ";
+                        }
+                    }
+                    
                     
                 }
 
-                //foreach (var acolito in lista)
-                //{
-                //    if(indice > 1)
-                //    {
-                //        acolitosQpodem += acolito.Nome + "/";
-                //    }
-                //    if(indice == 1)
-                //    {
-                //        acolitosQpodem += acolito.Nome;
-                //    }
-                //    indice--;
-                //}
-                //MessageBox.Show(acolitosQpodem);
                 return acolitosQpodem;
             }
 
@@ -369,14 +345,14 @@ namespace AppEscala
                             }
                             
                         }
-
+                    string vazio = "  ";
                         if(isDataRepetida == true)
                         {
-                        relProdutos.Add(new Produtos("", missa.Horario, acolitos, missa.Descricao, missa.Igreja));
+                        relProdutos.Add(new Produtos("", missa.Horario, vazio + acolitos, missa.Descricao, missa.Igreja));
                         }
                         else
                         {
-                        relProdutos.Add(new Produtos(dataConvertida.Day + "-" + diaConvertido, missa.Horario, acolitos, missa.Descricao, missa.Igreja));
+                        relProdutos.Add(new Produtos(dataConvertida.Day + "-" + diaConvertido, missa.Horario, vazio + acolitos, missa.Descricao, missa.Igreja));
                         }
 
                 }
